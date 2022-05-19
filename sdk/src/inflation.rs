1393abfe8ae7ded2c56124392796f27054124b2f
mundisnetwork <maintainers@mundis.io>
2022-05-19 13:34:21 +0300

    pass runtime tests

--- sdk/src/inflation.rs
+++ sdk/src/inflation.rs
@@ -109,6 +109,7 @@ impl Inflation {
 
 #[cfg(test)]
 mod tests {
+    use num_traits::Pow;
     use super::*;
 
     #[test]
@@ -120,9 +121,8 @@ mod tests {
 
         for year in &[0.1, 0.5, 1.0, DEFAULT_FOUNDATION_TERM, 100.0] {
             let total = inflation.total(*year);
-            assert_eq!(
-                total,
-                inflation.validator(*year) + inflation.foundation(*year)
+            assert!(
+                total - (inflation.validator(*year) + inflation.foundation(*year)) < 10.0.pow(-10)
             );
             assert!(total < last);
             assert!(total >= inflation.terminal);
