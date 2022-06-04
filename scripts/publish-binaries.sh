8739dfcda5c15daa143a616c46b0341758daf059
mundisnetwork <maintainers@mundis.io>
2022-06-04 08:57:32 +0300

    Add downloader script for Mundis binaries

--- scripts/publish-binaries.sh
+++ scripts/publish-binaries.sh
@@ -10,6 +10,10 @@ fi
 S3_DEST="s3://release.mundis.io"
 BINARIES=('mundis' 'mundis-faucet' 'mundis-genesis' 'mundis-gossip' 'mundis-keygen' 'mundis-ledger-tool' 'mundis-validator')
 
+cp -f "$SCRIPT_DIR/installer" /tmp/installer
+sed -i "s/MUNDIS_RELEASE=v0.0.0/MUNDIS_RELEASE=$GITHUB_TAG/g" /tmp/installer
+aws s3 cp /tmp/installer "$S3_DEST/$GITHUB_TAG/installer"
+rm -f /tmp/installer
 
 for binary in "${BINARIES[@]}"
 do
