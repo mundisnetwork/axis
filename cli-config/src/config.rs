03e06fbcb25d8155041af67bc13c761c81144935
mundisnetwork <maintainers@mundis.io>
2022-07-15 17:02:40 +0300

    set default RPC URL to devnet

--- cli-config/src/config.rs
+++ cli-config/src/config.rs
@@ -72,7 +72,7 @@ impl Default for Config {
             keypair_path.extend(&[".config", "mundis", "id.json"]);
             keypair_path.to_str().unwrap().to_string()
         };
-        let json_rpc_url = "https://api.mainnet.mundis.io".to_string();
+        let json_rpc_url = "https://api.devnet.mundis.io".to_string();
 
         // Empty websocket_url string indicates the client should
         // `Config::compute_websocket_url(&json_rpc_url)`
