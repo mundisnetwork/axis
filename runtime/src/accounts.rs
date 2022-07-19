7c5d40ffbb1f46e16596b5d24c53097fbe3e5457
mundisnetwork <maintainers@mundis.io>
2022-07-19 22:03:31 +0300

    Reject durable nonce transactions not signed by authority

--- runtime/src/accounts.rs
+++ runtime/src/accounts.rs
@@ -3114,7 +3114,8 @@ mod tests {
         assert!(nonce_account::verify_nonce_account(
             &collected_nonce_account,
             durable_nonce.as_hash(),
-        ));
+        )
+            .is_some());
     }
 
     #[test]
@@ -3220,7 +3221,8 @@ mod tests {
         assert!(nonce_account::verify_nonce_account(
             &collected_nonce_account,
             durable_nonce.as_hash()
-        ));
+        )
+            .is_some());
     }
 
     #[test]
